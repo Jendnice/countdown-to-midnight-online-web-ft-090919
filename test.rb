49f10c72c2e42c962fb75e4311cc00f937c6fa23
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-10 21:36:53 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -7,3 +7,14 @@ def countdown(number)
 end
 
 countdown(5)
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1 
+  end 
+  "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(5)
